484b45d10a6938cb2b3816fa89ccd117368e58c6
tejalkulkarni50-lab <tejalkulkarni50@gmail.com>
2026-03-05 13:57:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
-
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
